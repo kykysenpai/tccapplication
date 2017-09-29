f155d3e0ca2a838553f1fbaaa4ddf6e8a44e7af1
Kyky <kyrilltircher14@hotmail.com>
2017-09-29 23:02:15 +0200

    oui

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/www'));
-app.use(bodyParser.urlencod({extended : false}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
