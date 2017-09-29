fdb951ea1320191a7b2d8aec5cc7b74c5dbda335
Kyky <kyrilltircher14@hotmail.com>
2017-09-29 19:19:25 +0200

    oui

--- www/js/main.js
+++ www/js/main.js
@@ -4,9 +4,9 @@ $(function(){
   $('.toHide').hide();
   $('.admin').hide();
 
-  $('nav').find('li').each(function(){
+  $('#navbar').find('li').each(function(){
     $(this).click(function(){
-      $('nav').find('li').each(function(){
+      $('#navbar').find('li').each(function(){
         $(this).removeClass("active");
       });
       $(this).addClass("active");
