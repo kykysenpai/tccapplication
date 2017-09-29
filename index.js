8afcc1954898cf6f2f3fe49e7dba0b74d9bf231f
Kyky <kyrilltircher14@hotmail.com>
2017-09-29 23:18:22 +0200

    oui

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const session = require('express-session');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/www'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+app.use(session({
+	secret: 'TCC VAINCRA',
+	cookie:{
+		secure: true
+	}
+}));
 
 app.get('/', (req, res) => {
 	fs.readFile('www/index.html', function (err, html) {
@@ -15,8 +22,7 @@ app.get('/', (req, res) => {
 			throw err;
 		}
 		res.writeHeader(200, {"Content-Type": "text/html"});
-		res.write(html);
-		res.end();
+		res.send(html);
 	});
 });
 
@@ -25,10 +31,19 @@ app.post('/post', (req, res) => {
 	if(!action){
 		action = "none";
 	}
-	res.write(action);
-	res.end();
+	//res.send(action);
+	isLogged(req, resp);
+	resp.end();
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function isLogged(req, resp){
+	if(req.session){
+		resp.write("session : " + JSON.stringify(req.session));
+	}else{
+		resp.write("pas de session");
+	}
+}
