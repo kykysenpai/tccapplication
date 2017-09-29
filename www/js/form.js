7e73a5a4076c10bfdcf54bac54aaf1a3d00c63e8
Kyky <kyrilltircher14@hotmail.com>
2017-09-29 18:25:14 +0200

    oui

--- www/js/form.js
+++ www/js/form.js
@@ -1,19 +1,3 @@
 $(function(){
-    //main login in app
-    $('#mainModalButton').click(function(){
-      var map = formToJson("mainModalLogin");
-      $.ajax({
-        url : '/post',
-        type : 'POST',
-        data : {action:'mainLogin', valeurs:map},
-        success : function(){
-            console.log("success login");
-        },
-        error : function(){
-          console.log("error login");
-        }
-      });
-      //fin ajax
-    });
-    //fin click mainModalButton
+    
 });
--- www/js/main.js
+++ www/js/main.js
@@ -4,14 +4,4 @@ $(function(){
   $('.toHide').hide();
   $('.admin').hide();
 
-  // ouverture du modal
-  $('#mainNavConnexion').click(function(){
-    $('#mainModalDiv').dialog();
-  });
-
-  // ferme le modal après avoir appuyer sur "se Connecter"
-  $('#mainModalButton').click(function(){
-    $('#mainModalDiv').dialog("close");
-  });
-
 });
