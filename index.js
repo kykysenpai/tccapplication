119cb9b6b32f27165725063f215f22880b128467
kyky <kyrilltircher14@hotmail.com>
2017-09-29 14:19:24 +0200

    ndex

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express'), 
+const express = require('express'),
 app = express(),
 fs = require('fs'),
 http = require('http');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/www'));
+
 app.get('/', (req, res) => {
-	
+
 	fs.readFile('www/index.html', function (err, html) {
 		if (err) {
-			throw err; 
-		}  	
+			throw err;
+		}
 		res.writeHeader(200, {"Content-Type": "text/html"});
 		res.write(html);
 		res.end();
