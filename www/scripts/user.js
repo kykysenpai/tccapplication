324b302a1c2e5b72627a0dfbe75e118a42ea010c
kyky <kyrilltircher14@hotmail.com>
2017-10-02 12:39:14 +0200

    oui

---
+++ www/scripts/user.js
@@ -0,0 +1,23 @@
+function loadChatUser(id_user){
+  $.ajax({
+    url:'/post',
+    type:'POST',
+    data:{
+      action:'loadChatUser',
+      id_user:id_user
+    },
+    success: function(ret){
+      var ret = JSON.parse(ret);
+      gererOutput(ret.num, 'loading user info');
+      var map = ret.map;
+      $('#pageUser > h1[name=login]').text(map.login);
+      $('#pageUser li[name=surname]').text('Nom de famille : ' + map.surname);
+      $('#pageUser li[name=firstname]').text('Prenom : ' + map.firstname);
+      $('#pageUser li[name=login]').text('Pseudo : ' + map.login);
+      $('#pageUser li[name=email]').text('Email : ' + map.email);
+    },
+    error: function(ret){
+      gererOutput(2, 'loading user info');
+    }
+  });
+}
