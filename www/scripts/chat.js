1f9b6b15eac2dba27d064a39d97ac27e82cafdb1
Jonathan <jonathan.visiedo@gmail.com>
2017-10-02 22:10:32 +0200

    'current_users'

--- www/scripts/chat.js
+++ www/scripts/chat.js
@@ -7,11 +7,18 @@ $(function(){
     socket.on('chatMessage', function(msg){
         $('#chatMessages').append('<li class="list-group-item">[' + msg.date + '] ' + msg.user + ': ' + msg.msg +'</li>');
     });
-    console.log('hello');
-    socket.emit('current_users', null);
+
+    var connected_users;
     socket.on('current_users', function(data){
-        console.log(data);
+        connected_users = data
+        for(var id_user in connected_users){
+            $('#chatUsers').find('a').each(function(){
+               if($(this).attr('name') == id_user)
+                   $(this).addClass('active');
+            });
+        }
     });
+
 
     $.ajax({
         url: '/post',
@@ -23,11 +30,9 @@ $(function(){
             var ret = JSON.parse(ret);
             gererOutput(ret.num, 'loading chat users');
             for(var user in ret.map){
-                if(current_user_id == ret.map[user].id_user)
-                    $('#chatUsers').append('<a class="list-group-item active" name="'+ret.map[user].id_user+'">' + ret.map[user].login + '</a>');
-                else
-                    $('#chatUsers').append('<a class="list-group-item" name="'+ret.map[user].id_user+'">' + ret.map[user].login + '</a>');
+                $('#chatUsers').append('<a class="list-group-item" name="'+ret.map[user].id_user+'">' + ret.map[user].login + '</a>');
             }
+
             $('#chatUsers').find('a').each(function(){
                 $(this).click(function(){
                     var id_user = $(this).attr('name');
@@ -36,9 +41,11 @@ $(function(){
                     });
                 });
             });
+            socket.emit('current_users', null);
         },
         error: function(ret){
             gererOutput(2, 'loading chat users');
         }
     });
+
 });
