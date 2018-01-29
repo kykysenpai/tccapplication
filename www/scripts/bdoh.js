57704524fe950c5c6c2c4cfc5ccb2d1c28fc76eb
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-01-29 20:42:11 +0100

    test table

--- www/scripts/bdoh.js
+++ www/scripts/bdoh.js
@@ -1,20 +1,24 @@
+var currentID = 0;
+
 $(function() {
 	$('#tableTimerMarketplaceButton').click(function() {
-		var row = "<tr>";
-		row += "<td>" + $('#tableTimerMarketplace > input[name="nomItem"]').val() + "</td>";
-		row += "<td>" + $('#tableTimerMarketplace > input[name="nombreMinutes"]').val() + "</td>";
+		var row = '<tr id=tableTimerMarketplaceRow_"' + ++currentID + '">';
+		row += "<td>" + $('#tableTimerMarketplaceNomItem').val() + "</td>";
+		row += "<td>" + $('#tableTimerMarketplaceNombreMinutes').val() + "</td>";
 		row += "<td>" + "</td>";
 		row += "</tr>";
-		console.log(row);
 		$('#tableTimerMarketplace > tr:first').after(row);
+		startTimer($('#tableTimerMarketplaceNombreMinutes').val() * 60 * 1000);
 	})
 });
 
 function startTimer(interval) {
 	var x = setInterval(function() {
 		interval -= 1000;
+		localID = currentID;
 		if (interval <= 0) {
-			console.log("timeout");
+			//son
+			$('#tableTimerMarketplaceRow_' + localID).remove();
 			clearInterval(x);
 		}
 	}, 1000);
