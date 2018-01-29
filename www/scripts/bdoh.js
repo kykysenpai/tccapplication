5c3ff0ba222e1e3ac75309d1f4e46762a5ef3892
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-01-29 23:00:23 +0100

    td

--- www/scripts/bdoh.js
+++ www/scripts/bdoh.js
@@ -20,7 +20,7 @@ $(function() {
 function startTimerMarketPlace(interval, id) {
 	var x = setInterval(function() {
 		interval -= 1000;
-		$('#tableTimerMarketplace tr[name="tableTimerMarketplaceRow_' + id + '"]').text(interval / 1000);
+		$('#tableTimerMarketplace tr[name="tableTimerMarketplaceRow_' + id + '"] td[name="nombreMinutes"]').text(interval / 1000);
 		if (interval <= 0) {
 
 			clearInterval(x);
