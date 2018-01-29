7ab4c30839dd6eb8281778bfd9d1d813a54bef15
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-01-29 23:27:31 +0100

    min sec

--- www/scripts/bdoh.js
+++ www/scripts/bdoh.js
@@ -5,12 +5,11 @@ $(function() {
 		var row = '<tr name=tableTimerMarketplaceRow_' + (++currentID) + '>';
 		row += '<td name="nomItem">' + $('#tableTimerMarketplaceNomItem').val() + "</td>";
 		row += '<td name="nombreMinutes">' + $('#tableTimerMarketplaceNombreMinutes').val() + "</td>";
-		row += '<td><button class="tableTimerMarketPlaceButtonRemove" type="button">Supprimer</button></td>';
+		row += '<td><button class="tableTimerMarketPlaceButtonRemove btn" type="button">Supprimer</button></td>';
 		row += "</tr>";
 		$('#tableTimerMarketplace').append(row);
-		startTimerMarketPlace($('#tableTimerMarketplaceNombreMinutes').val() * 1000, currentID);
+		startTimerMarketPlace($('#tableTimerMarketplaceNombreMinutes').val() * 60 * 1000, currentID);
 		$('#tableTimerMarketplace > tr:last > td:last > button').click(function() {
-			console.log("hello");
 			$(this).closest('tr').remove();
 		})
 	});
@@ -19,7 +18,9 @@ $(function() {
 function startTimerMarketPlace(interval, id) {
 	var x = setInterval(function() {
 		interval -= 1000;
-		$('#tableTimerMarketplace tr[name="tableTimerMarketplaceRow_' + id + '"] td[name="nombreMinutes"]').text(interval / 1000);
+		min = (interval / 1000) % 60;
+		sec = interval - (min * 60);
+		$('#tableTimerMarketplace tr[name="tableTimerMarketplaceRow_' + id + '"] td[name="nombreMinutes"]').text(min + ':' + sec);
 		if (interval <= 0) {
 
 			clearInterval(x);
