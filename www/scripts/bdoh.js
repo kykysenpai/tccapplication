57630f053aa5a4051e92d741cff4338713d35aa7
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-01-29 22:40:16 +0100

    remove button

--- www/scripts/bdoh.js
+++ www/scripts/bdoh.js
@@ -5,22 +5,24 @@ $(function() {
 		var row = '<tr id=tableTimerMarketplaceRow_"' + ++currentID + '">';
 		row += '<td name="nomItem">' + $('#tableTimerMarketplaceNomItem').val() + "</td>";
 		row += '<td name="nombreMinutes">' + $('#tableTimerMarketplaceNombreMinutes').val() + "</td>";
-		row += "<td>" + "</td>";
+		row += '<td><button class="tableTimerMarketPlaceButtonRemove" type="button">Supprimer</button></td>';
 		row += "</tr>";
 		$('#tableTimerMarketplace > tr:first').after(row);
 		startTimer($('#tableTimerMarketplaceNombreMinutes').val() * 1000, currentID);
 	})
+
+	$('.tableTimerMarketPlaceButtonRemove').click(function() {
+		$(this).closest('tr').remove();
+	})
+
 });
 
 function startTimer(interval, id) {
 	var x = setInterval(function() {
 		interval -= 1000;
-		$('#tableTimerMarketplaceRow_' + id + ' > td[name="nombreMinutes"]').val(interval / 1000);
+		$('#tableTimerMarketplaceRow_' + id + ' td[name="nombreMinutes"]').val(interval / 1000);
 		if (interval <= 0) {
-			//son
-			$('#tableTimerMarketplace tr[id="tableTimerMarketplaceRow_' + id + '"]').each(function() {
-				$(this).remove();
-			});
+
 			clearInterval(x);
 		}
 	}, 1000);
